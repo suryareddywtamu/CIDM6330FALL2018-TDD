e69262d5e71d5de611301c095f23776ee1c2105a
Surya Reddy <suryareddy@Suryas-MacBook-Pro.local>
2018-10-04 20:42:42 -0500

    Added tests and new code

--- functional_tests.py
+++ functional_tests.py
@@ -37,11 +37,9 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')  
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)  
         self.fail('Finish the test!')  
 
 if __name__ == '__main__':  
