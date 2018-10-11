fef097e18708d15c8d96b75171660c7ea05e29ce
Surya Reddy <suryareddy@Suryas-MacBook-Pro.local>
2018-10-11 17:39:41 -0500

    Added functionality to remove explicit sleep

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,10 +2,11 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
+from selenium.common.exceptions import WebDriverException 
 
+MAX_WAIT = 10 
 
-class NewVisitorTest(unittest.TestCase):  
+class NewVisitorTest(LiveServerTestCase):  
 
     def setUp(self):  
         self.browser = webdriver.Firefox()
@@ -13,16 +14,24 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):  
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+         try:
+           table = self.browser.find_element_by_id('id_list_table')
+           rows = table.find_elements_by_tag_name('tr')
+           self.assertIn(row_text, [row.text for row in rows])
+           return
+         except(AssertionError,WebDriverException) as e:
+           if time.time()-start_time> MAX_WAIT:
+              raise e
+           time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):  
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         #self.browser.get('http://localhost:8000')
-	self.browser.get(self.live_server_url)
+        self.browser.get(self.live_server_url)
 
         header_text = self.browser.find_element_by_tag_name('h1').text  
         self.assertIn('To-Do', header_text)
@@ -41,8 +50,7 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)  
-        time.sleep(1)  
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1 Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -50,15 +58,12 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('2 Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1 Buy peacock feathers')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # She notices the page title and header mention to-do lists
         self.fail('Finish the test!')  
 
-if __name__ == '__main__':  
-    unittest.main(warnings='ignore')  
